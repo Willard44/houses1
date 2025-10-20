eca6d93ff49c8b56305fbcf45dc237037abc2580
Willard44 <willardantonio@gmail.com>
2025-10-20 09:34:16 -0600

    Sigue2

---
+++ launch_appcasa.ps1
@@ -0,0 +1,8 @@
+# Activar entorno virtual y lanzar Streamlit
+Set-Location "C:\Users\Acer\OneDrive\Documents\DATA\1 Analista de Data\Repositorios\Streamlit"
+
+# Activar entorno virtual
+.\.venv\Scripts\Activate.ps1
+
+# Ejecutar la aplicación
+streamlit run app.py
